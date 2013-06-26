9c74197403873ec7266892f742be9d4b7dc6efbb
lpratt <lpratt@oberlin.edu>
2013-06-26 09:01:48 -0400

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -45,14 +46,13 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
-  private
+  def feed
+    # This is preliminary. See "Following users" for the full implementation.
+    Micropost.where("user_id = ?", id)
+  end
 
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
+
+  private
 
     def correct_user
       @user = User.find(params[:id])
